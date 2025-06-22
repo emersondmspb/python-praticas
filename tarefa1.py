ba41fc83ade4092f3c967bee9c86878c8d4082e9
emersondmspb <dreiton@petrobras.com.br>
2025-06-22 00:53:49 -0300

    Add files via upload
    Faça um programa em Python que receba (entrada de dados) o valor correspondente ao lado de um quadrado, calcule e imprima (saída de dados) seu perímetro e sua área.
    
    Observação: a saída deve estar no formato: "perímetro: x - área: y"
    
    Abaixo um exemplo de como devem ser a entrada e saída de dados do programa:
    
    Exemplo:
    
    Entrada de Dados: 
    
    Digite o valor correspondente ao lado de um quadrado: 3
    
    Saída de Dados:
    
    perímetro: 12 - área: 9

---
+++ tarefa1.py
@@ -0,0 +1,6 @@
+lado=input('Digite o valor correspondente ao lado de um quadrado: ')
+ladofloat=float(lado)
+perimetro=ladofloat*4
+area=ladofloat*ladofloat
+print('perímetro: ',perimetro,' - área: ',area)
+
