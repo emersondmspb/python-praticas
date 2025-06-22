d77fe1417fd34fd6ff3ce4dbcafddb1cbe736b8c
emersondmspb <dreiton@petrobras.com.br>
2025-06-22 01:03:54 -0300

    Add files via upload

---
+++ tarefa2.py
@@ -0,0 +1,16 @@
+nota1=input('Digite a primeira nota: ')
+nota2=input('Digite a segunda nota: ')
+nota3=input('Digite a terceira nota: ')
+nota4=input('Digite a quarta nota: ')
+resultado=(float(nota1)+float(nota2)+float(nota3)+float(nota4))/4
+print('A média aritmética é ',resultado)
+
+#Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética. Observe o #exemplo abaixo:
+#Exemplo:
+#Entrada de Dados:
+#Digite a primeira nota: 4
+#Digite a segunda nota: 5
+#Digite a terceira nota: 6
+#Digite a quarta nota: 7
+#Saída de Dados:
+#A média aritmética é 5.5
